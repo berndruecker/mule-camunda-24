d1e27970b6e8351a5d02202600feec5d6c8cb490
Bernd Ruecker <bernd.ruecker@camunda.com>
2014-05-06 15:44:53 +0200

    first draft from discussion

---
+++ mule-camunda-24-app/src/main/java/com/camunda/example/mule/MuleProcessApplication.java
@@ -0,0 +1,9 @@
+package com.camunda.example.mule;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class MuleProcessApplication extends ServletProcessApplication {
+
+}
