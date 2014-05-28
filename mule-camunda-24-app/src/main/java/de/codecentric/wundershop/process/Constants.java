75ba0f50572132649e67ee9f3e6ea053c0df1a5b
Bernd Ruecker <bernd.ruecker@camunda.com>
2014-05-28 08:28:56 +0200

    added correltion

--- mule-camunda-24-app/src/main/java/de/codecentric/wundershop/process/Constants.java
+++ mule-camunda-24-app/src/main/java/de/codecentric/wundershop/process/Constants.java
@@ -3,5 +3,6 @@ package de.codecentric.wundershop.process;
 public class Constants {
   
   public static final String PROCESS_NAME = "mule-order-management";
+  public static final String MSG_PAYMENT_RECEIVED = "PAYMENT_RECEIVED";
 
 }
--- mule-camunda-24-app/src/main/java/de/codecentric/wundershop/transformers/SendPaymentReceivedToWorkflow.java
+++ mule-camunda-24-app/src/main/java/de/codecentric/wundershop/transformers/SendPaymentReceivedToWorkflow.java
@@ -1,16 +1,23 @@
 package de.codecentric.wundershop.transformers;
 
+import org.camunda.bpm.BpmPlatform;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
 
+import de.codecentric.wundershop.process.Constants;
+
 public class SendPaymentReceivedToWorkflow extends AbstractMessageTransformer {
 
-    @Override
-    public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
-	String id = (String) message.getPayload();
-	System.out.println("SendPaymentReceivedToWorkflow, id=" + id);
-	// TODO: Hier an camunda melden, dass Order mit id bezahlt ist.
-	return message;
-    }
+  @Override
+  public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
+    String orderId = (String) message.getPayload();
+    System.out.println("SendPaymentReceivedToWorkflow, id=" + orderId);
+    
+    BpmPlatform.getDefaultProcessEngine().getRuntimeService().createMessageCorrelation(Constants.MSG_PAYMENT_RECEIVED) //
+      .processInstanceBusinessKey(orderId) //
+      .correlate();
+
+    return message;
+  }
 }
